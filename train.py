685e358572ab6e7fd5eae6e8d2a4ba7e09b25f82
kgreyy <>
2022-11-25 17:22:16 +0800

    Modify model parameters

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+import os
 
 import torch.utils.data.dataloader as DataLoader
 
@@ -23,32 +24,34 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 
 
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    # Use deterministic cudnn algorithms
-    torch.backends.cudnn.deterministic = True
-    epochs = 10
-else:
-    device = torch.device("cpu")
-    epochs = 5
+if __name__=='__main__':
+    if torch.cuda.is_available():
+        # Use deterministic cudnn algorithms
+        torch.backends.cudnn.deterministic = True
+        epochs = 50
+    else:
+        device = torch.device("cpu")
+        epochs = 5
 
-data_dir = './data/'
+    # os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
-BATCH_SIZE=2
+    data_dir = './data/'
+    BATCH_SIZE=32
 
-dataset = dsets.ImageFolder(
-    root=data_dir,
-    transform=SimCLRTrainDataTransform(input_height=512)
-)
-dataloader = data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8)
+    with torch.cuda.device("cuda:0"):
+        dataset = dsets.ImageFolder(
+            root=data_dir,
+            transform=SimCLRTrainDataTransform(input_height=512)
+        )
+        dataloader = data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8)
 
 
-model = SimCLR( gpus=-1, batch_size=BATCH_SIZE, dataset=data_dir, num_samples=100, max_epochs=epochs)
+        model = SimCLR(gpus=1, num_samples=1000, batch_size=BATCH_SIZE, dataset=dataset, max_epochs=epochs)
 
+        print("Device: {}".format(torch.cuda.current_device()))
+        print("Epochs: {}".format(epochs))
 
-print("Device: {}".format(device))
-print("Epochs: {}".format(epochs))
+        # model = SimCLR(2, 5, 5, dataset, max_epochs=epochs)
+        trainer = pl.Trainer(accelerator="gpu", max_epochs=epochs, default_root_dir="./trained_models/")
+        trainer.fit(model, train_dataloaders=dataloader)
 
-model = SimCLR(2, 5, 5, dataset, max_epochs=epochs)
-trainer = pl.Trainer(accelerator="gpu", max_epochs=epochs, default_root_dir="/trained_models/")
-trainer.fit(model, train_dataloaders=dataloader)
